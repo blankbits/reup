5dbc40503f003d5ad769d6b3c8ee11490b4810a0
Peter Brandt <peterbrandt84@gmail.com>
2020-06-29 15:53:17 -0700

    Add initial polygon downloader files

---
+++ polygon_downloader.py
@@ -0,0 +1,324 @@
+#!/usr/bin/env python3
+"""Contains a main function which downloads historical quote and trade data from
+Polygon, does some basic validation, and dumps both the raw API responses and
+CSV formatted data to disk.
+
+It defaults to using the config specified in polygon_downloader_config.yaml, but
+this can be overridden via a command line arg.
+
+Example:
+    ./main.py --config_file custom_config.yaml
+
+"""
+import argparse
+import datetime
+import enum
+import gzip
+import io
+import logging
+import logging.config
+import pickle
+import sys
+import threading
+
+import pandas as pd
+import polygon
+import pytz
+import yaml
+
+
+class HistoricalDataType(enum.Enum):
+    """Enum for the type of historical data being handled.
+
+    """
+    QUOTES = enum.auto()
+    TRADES = enum.auto()
+
+
+class AsyncWriteFileGzip(threading.Thread):
+    """Writes and gzips a file on a separate thread.
+
+    """
+    def __init__(self, data: bytes, path: str) -> None:
+        """Initialize the data and path to write.
+
+        Args:
+            data: Data to write.
+            path: Path to write.
+
+        """
+        threading.Thread.__init__(self)
+        self._data = data
+        self._path = path
+
+    def run(self) -> None:
+        """Write and gzip file.
+
+        Raises:
+            SystemExit: An error occurred when trying to write the file.
+
+        """
+        logger = logging.getLogger(__name__)
+        try:
+            logger.info('Writing file | path: %s', self._path)
+            with gzip.open(self._path, 'wb') as file_object:
+                file_object.write(self._data)
+        except OSError:
+            logger.error('Write failed')
+            sys.exit(1)
+
+
+def fetch_responses(historical_data_type: HistoricalDataType, api_key: str,
+                    response_limit: int, symbol: str, date: str) -> list:
+    """Use the REST API to get historical data, querying as many times as needed
+    to get all the data for a single symbol and date.
+
+    Args:
+        historical_data_type: Enum indicating which type of data to fetch.
+        api_key: Polygon-issued API key.
+        response_limit: Max records returned per API request.
+        symbol: Ticker symbol.
+        date: YYYY-MM-DD format.
+
+    Returns:
+        List of the raw API responses.
+
+    Raises:
+        SystemExit: An API call wasn't successful.
+
+    """
+    logger = logging.getLogger(__name__)
+    responses = []
+    min_timestamp = 0
+    client = polygon.RESTClient(api_key)
+
+    while True:
+        logger.info(
+            'Fetching responses | %s', 'historical_data_type: {}, ticker: {}'
+            ', date: {}, timestamp: {}, limit: {}'.format(
+                historical_data_type.name, symbol, date, min_timestamp,
+                response_limit))
+
+        if historical_data_type is HistoricalDataType.QUOTES:
+            responses.append(
+                client.historic_n___bbo_quotes_v2(ticker=symbol,
+                                                  date=date,
+                                                  timestamp=min_timestamp,
+                                                  limit=response_limit))
+        elif historical_data_type is HistoricalDataType.TRADES:
+            responses.append(
+                client.historic_trades_v2(ticker=symbol,
+                                          date=date,
+                                          timestamp=min_timestamp,
+                                          limit=response_limit))
+
+        if not responses[-1].success:
+            logger.error('Fetch failed')
+            sys.exit(1)
+
+        logger.info('Fetch succeeded | %s',
+                    'results_count: {}'.format(responses[-1].results_count))
+        if responses[-1].results_count < response_limit:
+            break
+
+        min_timestamp = responses[-1].results[-1]['t']
+
+    return responses
+
+
+def generate_csv(historical_data_type: HistoricalDataType,
+                 responses: list) -> str:
+    """Convert the raw API responses for historical data into CSV format,
+    discarding extraneous data.
+
+    Args:
+        historical_data_type: Enum indicating the type of responses.
+        responses: Raw API responses for a single symbol and date.
+
+    Returns:
+        CSV-formatted string.
+
+    Raises:
+        SystemExit: Unexpected lack of overlap between API responses.
+
+    """
+    logger = logging.getLogger(__name__)
+
+    if historical_data_type is HistoricalDataType.QUOTES:
+        csv_data = [
+            'timestamp,bid_price,bid_size,bid_exchange,'
+            'ask_price,ask_size,ask_exchange,conditions,indicators'
+        ]
+    elif historical_data_type is HistoricalDataType.TRADES:
+        csv_data = ['timestamp,price,size,exchange,conditions']
+
+    logger.info('Generating CSV | historical_data_type: %s',
+                historical_data_type.name)
+    for i, response in enumerate(responses):
+        for j, result in enumerate(response.results):
+            if i > 0 and j == 0:
+                # Due to API behavior, the first result for the current response
+                # is identical to the last result of the previous response
+                # (unless this is the first response). This logic discards the
+                # duplicate results.
+                last_result = responses[i - 1].results[-1]
+                if (last_result['t'] == result['t']
+                        and last_result['q'] == result['q']):
+                    pass
+                else:
+                    logger.error('No overlap in response timestamps')
+                    sys.exit(1)
+            else:
+                if historical_data_type is HistoricalDataType.QUOTES:
+                    csv_data.append('{},{},{},{},{},{},{},{},{}'.format(
+                        result['t'], result['p'], result['s'], result['x'],
+                        result['P'], result['S'], result['X'],
+                        ' '.join(map(str, result.get('c', []))),
+                        ' '.join(map(str, result.get('i', [])))))
+                elif historical_data_type is HistoricalDataType.TRADES:
+                    csv_data.append('{},{},{},{},{}'.format(
+                        result['t'], result['p'], result['s'], result['x'],
+                        ' '.join(map(str, result.get('c', [])))))
+
+    return '\n'.join(csv_data) + '\n'
+
+
+def validate_timestamps(csv_data: str, time_zone: datetime.tzinfo,
+                        max_time_start: datetime.time,
+                        min_time_end: datetime.time,
+                        max_time_delta: datetime.timedelta) -> None:
+    """Perform a few sanity checks on the timestamps in CSV data.
+
+    Args:
+        csv_data: CSV-formatted string.
+        time_zone: Time zone for converting Unix time.
+        max_time_start: The latest time the data should begin.
+        min_time_end: The earliest time the data should stop.
+        max_time_delta: The longest time gap that is allowed.
+
+    Raises:
+        SystemExit: Validation failed.
+
+    """
+    logger = logging.getLogger(__name__)
+
+    # Create dataframe from CSV timestamps.
+    logger.info('Validating CSV timestamps')
+    df = pd.read_csv(io.StringIO(csv_data),
+                     usecols=['timestamp'],
+                     dtype={'timestamp': 'int64'})
+
+    # Populate helper columns.
+    df['datetime'] = [
+        datetime.datetime.fromtimestamp(t, time_zone)
+        for t in df['timestamp'] / 10.0**9
+    ]
+    df['time'] = [t.time() for t in df['datetime']]
+    df['timedelta'] = (df['datetime'] - df['datetime'].shift()).fillna(
+        pd.Timedelta(0))
+
+    if sum(df.duplicated()) > 0:
+        logger.error('Validation failed for duplicates check')
+        sys.exit(1)
+
+    if df['time'].iloc[0] > max_time_start:
+        logger.error('Validation failed for max_time_start')
+        sys.exit(1)
+
+    if df['time'].iloc[-1] < min_time_end:
+        logger.error('Validation failed for min_time_end')
+        sys.exit(1)
+
+    # Check max time delta, only considering timestamps after max start and
+    # before min end times.
+    if max(df['timedelta'][(df['time'] >= max_time_start)
+                           & (df['time'] <= min_time_end)]) > max_time_delta:
+        logger.error('Validation failed for max_time_delta')
+        sys.exit(1)
+
+
+def main():
+    """Begin executing main logic of the script.
+
+    """
+    # Parse command line args and load config.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_file',
+                        metavar='FILE',
+                        help='config YAML',
+                        default='polygon_downloader_config.yaml')
+    args = parser.parse_args()
+    with open(args.config_file, 'r') as config_file:
+        config = yaml.safe_load(config_file.read())
+
+    # Initialize date and time variables from config.
+    time_zone = pytz.timezone(config['time_zone'])
+    max_time_start = datetime.time(hour=config['max_start_hour'],
+                                   minute=config['max_start_minute'])
+    min_time_end = datetime.time(hour=config['min_end_hour'],
+                                 minute=config['min_end_minute'])
+    max_time_delta = datetime.timedelta(minutes=config['max_delta_minutes'])
+
+    # Setup logger.
+    logging.config.dictConfig(config['logging'])
+
+    # Threads for writing files async.
+    threads = []
+
+    # Fetch raw quotes API responses if needed for writing files.
+    if 'quotes_responses_path' in config or 'quotes_csv_path' in config:
+        quotes_responses = fetch_responses(HistoricalDataType.QUOTES,
+                                           config['api_key'],
+                                           config['response_limit'],
+                                           config['symbol'], config['date'])
+
+    # Write raw quotes API responses to file.
+    if 'quotes_responses_path' in config:
+        threads.append(
+            AsyncWriteFileGzip(pickle.dumps(quotes_responses),
+                               config['quotes_responses_path']))
+        threads[-1].start()
+
+    # Generate quotes CSV from responses, validate, and write to file.
+    if 'quotes_csv_path' in config:
+        quotes_csv_data = generate_csv(HistoricalDataType.QUOTES,
+                                       quotes_responses)
+        validate_timestamps(quotes_csv_data, time_zone, max_time_start,
+                            min_time_end, max_time_delta)
+        threads.append(
+            AsyncWriteFileGzip(quotes_csv_data.encode(),
+                               config['quotes_csv_path']))
+        threads[-1].start()
+
+    # Fetch raw trades API responses if needed for writing files.
+    if 'trades_responses_path' in config or 'trades_csv_path' in config:
+        trades_responses = fetch_responses(HistoricalDataType.TRADES,
+                                           config['api_key'],
+                                           config['response_limit'],
+                                           config['symbol'], config['date'])
+
+    # Write raw trades API responses to file.
+    if 'trades_responses_path' in config:
+        threads.append(
+            AsyncWriteFileGzip(pickle.dumps(trades_responses),
+                               config['trades_responses_path']))
+        threads[-1].start()
+
+    # Generate trades CSV from responses, validate, and write to file.
+    if 'trades_csv_path' in config:
+        trades_csv_data = generate_csv(HistoricalDataType.TRADES,
+                                       trades_responses)
+        validate_timestamps(trades_csv_data, time_zone, max_time_start,
+                            min_time_end, max_time_delta)
+        threads.append(
+            AsyncWriteFileGzip(trades_csv_data.encode(),
+                               config['trades_csv_path']))
+        threads[-1].start()
+
+    # Wait for files to finish writing async.
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    main()
