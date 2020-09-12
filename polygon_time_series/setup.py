3a2a58121b9fa1923ddbfa715ae7b954f2a5f8c5
Peter Brandt <peterbrandt84@gmail.com>
2020-09-12 22:34:23 +0000

    Add pytest and package skeleton.

---
+++ polygon_time_series/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="polygon_time_series", packages=find_packages())
---
+++ polygon_time_series/tests/test_polygon_time_series.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import pytest
+
+def test_stub():
+    assert True
