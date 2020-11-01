5c9c36b85caf83131f994421b62d2cac09fa5ed6
Peter Brandt <peterbrandt84@gmail.com>
2020-11-01 06:21:15 +0000

    Add tests for features day.

--- features_day/tests/test_features_day.py
+++ features_day/tests/test_features_day.py
@@ -2,7 +2,6 @@
 """Tests the generation of features that are static for the entire day.
 
 """
-import numpy as np
 import pandas as pd
 import pytest
 
@@ -20,15 +19,41 @@ def fixture_time_series_df():
 @pytest.fixture(name='output_df', scope='module')
 def fixture_output_df(time_series_df):
     """Create output data frame from test data.
+
     """
     return fd.get_output_df(time_series_df, '2020-04-01')
 
 
-def test_output_df_timestamp_delta(output_df):
-    """
+def test_output_df_high_price(output_df):
+    """Test that high price is populated correctly.
 
     """
-    # deltas = (seconds_df['timestamp'] - seconds_df['timestamp'].shift())[1:]
-    # assert np.abs(1.0 - deltas.min()) <= np.finfo(np.float64).eps
-    # assert np.abs(1.0 - deltas.max()) <= np.finfo(np.float64).eps
-    assert True
+    assert output_df.at[0, 'high_price'] == 248.2
+
+
+def test_output_df_low_price(output_df):
+    """Test that low price is populated correctly.
+
+    """
+    assert output_df.at[0, 'low_price'] == 247.92
+
+
+def test_output_df_vwap(output_df):
+    """Test that vwap is populated correctly.
+
+    """
+    assert output_df.at[0, 'vwap'] == 247.97639356156992
+
+
+def test_output_df_volume_total(output_df):
+    """Test that volume total is populated correctly.
+
+    """
+    assert output_df.at[0, 'volume_total'] == 917755
+
+
+def test_output_df_weekday(output_df):
+    """Test that weekday is populated correctly.
+
+    """
+    assert output_df.at[0, 'weekday'] == 2
