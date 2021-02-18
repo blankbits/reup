240fad2f47ed07706be34953d7762ef3dd14c97b
Peter Brandt <peterbrandt84@gmail.com>
2021-02-18 08:07:09 +0000

    Complete initial implementation of full pipeline functionality, and address all linter issues.

--- common/pipeline.py
+++ common/pipeline.py
@@ -8,7 +8,7 @@ import logging
 import logging.config
 import sys
 import time
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import yaml
 
@@ -18,9 +18,20 @@ import polygon_time_series.lambda_invoke as pts_lambda_invoke
 
 
 def run_stage(lambda_invoke: reup_utils.LambdaInvokeSimple,
-              date_symbol_dict: Dict[str, List[str]],
-              max_retry_count: int,
+              date_symbol_dict: Dict[str, List[str]], max_retry_count: int,
               retry_seconds: int) -> None:
+    """Run a single pipeline stage for all dates and symbols specified. If the
+    initial run doesn't fully complete, and there are still pending dates and
+    symbols, retry up to a max number of retries and exit upon failure.
+
+    Args:
+        lambda_invoke: Object representing the specific Lambda function to
+            invoke and related behavior.
+        date_symbol_dict: Dict with date keys and symbol list values.
+        max_retry_count: Maximum number of times to retry incomplete stage.
+        retry_seconds: Number of seconds to sleep before each retry.
+
+    """
     logger = logging.getLogger(__name__)
     retry_count = 0
     while retry_count <= max_retry_count:
@@ -30,8 +41,10 @@ def run_stage(lambda_invoke: reup_utils.LambdaInvokeSimple,
             return
 
         lambda_invoke.run(pending_date_symbol_dict)
-        logger.info('Sleeping | %s', 'retry_count:{}, retry_seconds:{}'.format(
-            retry_count, retry_seconds))
+        logger.info(
+            'Sleeping | %s',
+            'retry_count:{}, retry_seconds:{}'.format(retry_count,
+                                                      retry_seconds))
         time.sleep(retry_seconds)
         retry_count += 1
 
@@ -49,7 +62,9 @@ def main():
     """
     # Parse command line args.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config_file', metavar='FILE', help='config YAML',
+    parser.add_argument('--config_file',
+                        metavar='FILE',
+                        help='config YAML',
                         default='pipeline_config.yaml')
     args = parser.parse_args()
 
@@ -68,75 +83,48 @@ def main():
         universes[s3_universe_prefix] = reup_utils.Universe(
             config['s3_bucket'], s3_universe_prefix)
 
+    # Initialize LambdaInvoke objects for each pipeline stage and add to list in
+    # the order that the pipeline stages will run.
+    lambda_invokers: List[Tuple[str, reup_utils.LambdaInvokeSimple]] = []
+    lambda_invokers.append(
+        ('polygon_tick_data',
+         ptd_lambda_invoke.LambdaInvoke(
+             config['polygon_tick_data']['lambda_invoke_config'],
+             config['polygon_tick_data']['lambda_event'])))
+    lambda_invokers.append(
+        ('polygon_time_series',
+         pts_lambda_invoke.LambdaInvoke(
+             config['polygon_time_series']['lambda_invoke_config'],
+             config['polygon_time_series']['lambda_event'])))
+    lambda_invokers.append(
+        ('features_second',
+         reup_utils.LambdaInvokeSimple(
+             config['features_second']['lambda_invoke_config'],
+             config['features_second']['lambda_event'])))
+    lambda_invokers.append(('features_day',
+                            reup_utils.LambdaInvokeSimple(
+                                config['features_day']['lambda_invoke_config'],
+                                config['features_day']['lambda_event'])))
+
     # Loop thru dates and run full pipeline for each date.
     for date in config['dates']:
+        # Get list of symbols from all universes.
         symbol_list: List[str] = []
-        for s3_universe_prefix in config['s3_universe_prefixes']:
-            symbol_list.extend(
-                universes[s3_universe_prefix].get_symbol_list(date))
+        for name, universe in universes.items():
+            logger.info('Getting universe symbols | %s',
+                        'date:{}, name:{}'.format(date, name))
+            symbol_list.extend(universe.get_symbol_list(date))
 
+        # Remove duplicate symbols.
         symbol_list = sorted(list(set(symbol_list)))
 
-        # Run polygon tick data.
-        logger.info('Starting pipeline stage for polygon_tick_data | %s',
-                    'date:{}'.format(date))
-        lambda_invoke = ptd_lambda_invoke.LambdaInvoke(
-            '../polygon_tick_data/polygon_tick_data/lambda_invoke_config.yaml',
-            '../polygon_tick_data/polygon_tick_data/lambda_event.json')
-        run_stage(lambda_invoke, {date: symbol_list}, 3, 30)
-
-
-
-    # Run polygon tick data.
-    # logger.info('Starting pipeline stage for polygon_tick_data')
-    # lambda_invoke = ptd_lambda_invoke.LambdaInvoke(
-    #     '../polygon_tick_data/polygon_tick_data/lambda_invoke_config.yaml',
-    #     '../polygon_tick_data/polygon_tick_data/lambda_event.json')
-    # run_stage(lambda_invoke, date_symbol_dict, 3, 30)
-
-
-    # # Run polygon tick data.
-    # lambda_invoke = ptd_lambda_invoke.LambdaInvoke(
-    #     '../polygon_tick_data/polygon_tick_data/lambda_invoke_config.yaml',
-    #     '../polygon_tick_data/polygon_tick_data/lambda_event.json')
-    # pending_date_symbol_dict = lambda_invoke.get_pending_invocations(
-    #     date_symbol_dict)
-    # print(pending_date_symbol_dict)
-    # if len(pending_date_symbol_dict) > 0:
-    #     lambda_invoke.run(pending_date_symbol_dict)
-
-
-    # # Run polygon time series.
-    # lambda_invoke = pts_lambda_invoke.LambdaInvoke(
-    #     '../polygon_time_series/polygon_time_series/lambda_invoke_config.yaml',
-    #     '../polygon_time_series/polygon_time_series/lambda_event.json')
-    # pending_date_symbol_dict = lambda_invoke.get_pending_invocations(
-    #     date_symbol_dict)
-    # print(pending_date_symbol_dict)
-    # if len(pending_date_symbol_dict) > 0:
-    #     lambda_invoke.run(pending_date_symbol_dict)
-
-
-    # # Run features second.
-    # lambda_invoke = reup_utils.LambdaInvokeSimple(
-    #     '../features_second/features_second/lambda_invoke_config.yaml',
-    #     '../features_second/features_second/lambda_event.json')
-    # pending_date_symbol_dict = lambda_invoke.get_pending_invocations(
-    #     date_symbol_dict)
-    # print(pending_date_symbol_dict)
-    # if len(pending_date_symbol_dict) > 0:
-    #     lambda_invoke.run(pending_date_symbol_dict)
-
-
-    # # Run features day.
-    # lambda_invoke = reup_utils.LambdaInvokeSimple(
-    #     '../features_day/features_day/lambda_invoke_config.yaml',
-    #     '../features_day/features_day/lambda_event.json')
-    # pending_date_symbol_dict = lambda_invoke.get_pending_invocations(
-    #     date_symbol_dict)
-    # print(pending_date_symbol_dict)
-    # if len(pending_date_symbol_dict) > 0:
-    #     lambda_invoke.run(pending_date_symbol_dict)
+        # Run all pipeline stages in order.
+        for name, lambda_invoke in lambda_invokers:
+            logger.info('Starting pipeline stage | %s',
+                        'date:{}, name:{}'.format(date, name))
+            run_stage(lambda_invoke, {date: symbol_list},
+                      config[name]['max_retry_count'],
+                      config[name]['retry_seconds'])
 
 
 # If in top-level script environment, run main().
